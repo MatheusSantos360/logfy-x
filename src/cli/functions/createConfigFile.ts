bfba96b132f1fb9513f8086a4eeb7806bb88ae2a
Matheus Santos <matheusworkcontact@gmail.com>
2024-11-16 16:25:04 -0300

    refactor: logfy-x init command

--- src/cli/functions/createConfigFile.ts
+++ src/cli/functions/createConfigFile.ts
@@ -2,36 +2,25 @@ import { writeFile } from "fs";
 import path from "path";
 import logfy, { bgGreen, bgRed, bold, green, red } from "../../index";
 import { configFileName } from "../../types/config.json";
+import configFile from "../config.json";
 import { ConfigFileExists } from "./configFileExists";
-import configFile from "../config.json"
 
 export const createConfigFile = () => {
   if (ConfigFileExists()) {
-    logfy(
-      `${bgRed(" ❌ " + bold(`ALREADY EXISTS `))}\n${red(
-        bold(">"),
-      )} The Logfy-X config file (${configFileName}) already exists.`,
-      { style: "underline" },
-    );
+    logfy(`${bgRed(`\n ❌ ${bold(`ALREADY EXISTS `)}`)}\n${red(bold("  >"))} The Logfy-X config file (${configFileName}) already exists.`, {
+      style: "underline",
+    });
     return;
   }
 
   writeFile(path.join(process.cwd(), configFileName), JSON.stringify(configFile, null, 2), (error) => {
     if (error) {
-      logfy(
-        `${bgRed(" ❌ " + bold(`ERROR `))}\n ${red(
-          bold(">"),
-        )} Logfy-X config file created (${configFileName}): ${"error.message"}`,
-        { style: "underline" },
-      );
+      logfy(`${bgRed("\n ❌ " + bold(`ERROR `))}\n ${red(bold("  >"))} Logfy-X config file created (${configFileName}): ${"error.message"}`, {
+        style: "underline",
+      });
       return;
     }
 
-    logfy(
-      `${bgGreen(" 🎉 " + bold(`CREATED! `))}\n${green(
-        bold(">"),
-      )} Logfy-X config file created (${configFileName}).`,
-      { style: "underline" },
-    );
+    logfy(`${bgGreen("\n 🎉 " + bold(`CREATED! `))}\n${green(bold("  >"))} Logfy-X config file created (${configFileName}).`, { style: "underline" });
   });
 };
