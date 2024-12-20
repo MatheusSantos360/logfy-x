5abf2e930b18fef47e6cd04cc3cbc9abd245f6c8
Matheus dos Santos Paixão <matheusworkcontact@gmail.com>
2024-12-20 22:23:32 +0000

    doc: add jsdoc for info()

--- src/functions/info.ts
+++ src/functions/info.ts
@@ -1,6 +1,22 @@
 import { bgBlue, blue, bold } from "picocolors";
 import { formatNestedMessages } from "./utils/formatNestedMessages";
 
+/**
+ * Logs formatted informational messages to the console.
+ * @param {string} title - The title of the information message.
+ * @param {string | (string | string[])[]} messages - The message or array of messages.
+ * @example
+ * // Log a simple message
+ * info("Update", "The process was successful.");
+ *
+ * @example
+ * // Log nested messages
+ * info("Update", [
+ *   "The process was successful.",
+ *   ["Step 1: Completed", "Step 2: Completed"],
+ *   "Final step: Completed"
+ * ]);
+ */
 export const info = (title: string, messages: string | (string | string[])[]) => {
   if (typeof messages === "string") {
     const info = `${bgBlue(bold(` ℹ️  ${title} `))} ${blue(messages)}`;
